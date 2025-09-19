a6164f41c72e3954c10eef6b798da4bd06ae6cdc
yujinnn2 <yujinee111@gmail.com>
2025-09-19 13:24:59 +0900

    edited

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false,
+  devIndicators: { buildActivity: false },
+  output: "export",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      { protocol: "https", hostname: "raw.githubusercontent.com" },
+      { protocol: "https", hostname: "github.com" },
+      { protocol: "https", hostname: "user-images.githubusercontent.com" }
+    ]
+  }
 };
 
 export default nextConfig;
-
-
-module.exports = {
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "raw.githubusercontent.com" },
-      { protocol: "https", hostname: "github.com" },              // user-attachments, ?raw=1
-      { protocol: "https", hostname: "user-images.githubusercontent.com" },
-    ],
-  },
-};
